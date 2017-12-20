9a82ed49f11c5aed2521d2e6d4c1787a85b9e5f5
Travis Surgnier <surgie@gmail.com>
2017-12-19 19:38:41 -0500

    Simple Math

--- Ch2/Program.fs
+++ Ch2/Program.fs
@@ -5,15 +5,19 @@
 Challenges to revisit:
 - Counting the number of characters #2 -> Update UI as user types length
 - Printing Quotes -> Lists of Data
+- Mad Lib -> Branching Story
+- Simple Math -> Break program into functions (Ch5), Implemenet as Gui
 
 *)
 
 let main() =
-    SayingHello.sayingHello''()
-    CountingCharacters.countingNumberOfCharacters'()
-    PrintingQuotes.printingQuote()
-    MadLib.simple()
-    MadLib.challengeOne()
+    //SayingHello.sayingHello''()
+    // CountingCharacters.countingNumberOfCharacters'()
+    // PrintingQuotes.printingQuote()
+    // MadLib.simple()
+    // MadLib.challengeOne()
+    SimpleMath.simple()
+    SimpleMath.challengeOne()
 
     printfn "Press any key to continue..."
     Console.ReadLine() |> ignore
---
+++ Ch2/SimpleMath.fs
@@ -0,0 +1,45 @@
+﻿module SimpleMath
+
+open System
+
+let validation num =
+    match num with
+    | num when num < 0 -> false
+    | _ -> true
+
+let simple() =
+    printf "What is the first number? "
+    let first = Console.ReadLine()
+
+    printf "What is the second number? "
+    let second = Console.ReadLine()
+
+    let x = first |> int
+    let y = second |> int
+
+    printfn "%i + %i = %i\n%i - %i = %i\n%i * %i = %i\n%i / %i = %i" x y (x + y) x y (x - y) x y (x * y) x y (x / y)
+
+
+let challengeOne() =
+    printf "What is the first number? "
+    let first = Console.ReadLine()
+
+    printf "What is the second number? "
+    let second = Console.ReadLine()
+
+    let x =
+        try
+            first |> int
+        with
+            | ex -> printfn "Can't continue, %s is not a number." first; -1
+
+    let y =
+        try
+            second |> int
+        with
+            | ex -> printfn "Can't continue, %s is not a number." second; -1
+
+    let isValid = validation x && validation y
+    match isValid with
+        | true -> printfn "%i + %i = %i\n%i - %i = %i\n%i * %i = %i\n%i / %i = %i" x y (x + y) x y (x - y) x y (x * y) x y (x / y)
+        | false -> printfn "Numbers are invalid. They must be numeric and greater than 0."
